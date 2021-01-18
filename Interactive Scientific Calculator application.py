2eb720359d82ed25626d02c02ad9f0d165f028e6
rhishi8 <52154236+rhishi8@users.noreply.github.com>
2021-01-18 13:53:00 +0530

    Cursor Support
    Numbers & Symbols get added on the position of cursor.

--- Interactive Scientific Calculator application.py
+++ Interactive Scientific Calculator application.py
@@ -19,49 +19,49 @@ btn_h = 1
 btn_w = 4
 
 def f_ln():
-    entry.insert(tk.END, 'ln(')
+    entry.insert(tk.INSERT, 'ln(')
 
 ln_btn = tk.Button(text = 'ln', command = f_ln, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[0], row[0], window = ln_btn)
 
 def f_sin():
-    entry.insert(tk.END, 'sin(')
+    entry.insert(tk.INSERT, 'sin(')
 
 sin_btn = tk.Button(text = 'sin', command = f_sin, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[1], row[0], window = sin_btn)
 
 def f_cos():
-    entry.insert(tk.END, 'cos(')
+    entry.insert(tk.INSERT, 'cos(')
 
 cos_btn = tk.Button(text = 'cos', command = f_cos, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[2], row[0], window = cos_btn)
 
 def f_tan():
-    entry.insert(tk.END, 'tan(')
+    entry.insert(tk.INSERT, 'tan(')
 
 tan_btn = tk.Button(text = 'tan', command = f_tan, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[3], row[0], window = tan_btn)
 
 def f_pi():
-    entry.insert(tk.END, '\u03c0')
+    entry.insert(tk.INSERT, '\u03c0')
 
 pov_btn = tk.Button(text = '\u03c0', command = f_pi, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[4], row[0], window = pov_btn)
 
 def n_7():
-    entry.insert(tk.END, '7')
+    entry.insert(tk.INSERT, '7')
 
 n7_btn = tk.Button(text = '7', command = n_7, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[0], row[1], window = n7_btn)
 
 def n_8():
-    entry.insert(tk.END, '8')
+    entry.insert(tk.INSERT, '8')
 
 n8_btn = tk.Button(text = '8', command = n_8, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[1], row[1], window = n8_btn)
 
 def n_9():
-    entry.insert(tk.END, '9')
+    entry.insert(tk.INSERT, '9')
 
 n9_btn = tk.Button(text = '9', command = n_9, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[2], row[1], window = n9_btn)
@@ -80,73 +80,73 @@ AC_btn = tk.Button(text = 'AC', command = AC, font = btn_font, height = btn_h, w
 cvs.create_window(col[4], row[1], window = AC_btn)
 
 def n_4():
-    entry.insert(tk.END, '4')
+    entry.insert(tk.INSERT, '4')
 
 n4_btn = tk.Button(text = '4', command = n_4, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[0], row[2], window = n4_btn)
 
 def n_5():
-    entry.insert(tk.END, '5')
+    entry.insert(tk.INSERT, '5')
 
 n5_btn = tk.Button(text = '5', command = n_5, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[1], row[2], window = n5_btn)
 
 def n_6():
-    entry.insert(tk.END, '6')
+    entry.insert(tk.INSERT, '6')
 
 n6_btn = tk.Button(text = '6', command = n_6, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[2], row[2], window = n6_btn)
 
 def plus():
-    entry.insert(tk.END, '+')
+    entry.insert(tk.INSERT, '+')
 
 plus_btn = tk.Button(text = '+', command = plus, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[3], row[2], window = plus_btn)
 
 def minus():
-    entry.insert(tk.END, '-')
+    entry.insert(tk.INSERT, '-')
 
 minus_btn = tk.Button(text = '-', command = minus, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[4], row[2], window = minus_btn)
 
 def n_1():
-    entry.insert(tk.END, '1')
+    entry.insert(tk.INSERT, '1')
 
 n1_btn = tk.Button(text = '1', command = n_1, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[0], row[3], window = n1_btn)
 
 def n_2():
-    entry.insert(tk.END, '2')
+    entry.insert(tk.INSERT, '2')
 
 n2_btn = tk.Button(text = '2', command = n_2, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[1], row[3], window = n2_btn)
 
 def n_3():
-    entry.insert(tk.END, '3')
+    entry.insert(tk.INSERT, '3')
 
 n3_btn = tk.Button(text = '3', command = n_3, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[2], row[3], window = n3_btn)
 
 def multiply():
-    entry.insert(tk.END, '\u00d7')
+    entry.insert(tk.INSERT, '\u00d7')
 
 multiply_btn = tk.Button(text = '\u00D7', command = multiply, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[3], row[3], window = multiply_btn)
 
 def divide():
-    entry.insert(tk.END, '\u00f7')
+    entry.insert(tk.INSERT, '\u00f7')
 
 divide_btn = tk.Button(text = '\u00F7', command = divide, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[4], row[3], window = divide_btn)
 
 def n_0():
-    entry.insert(tk.END, '0')
+    entry.insert(tk.INSERT, '0')
 
 n0_btn = tk.Button(text = '0', command = n_0, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[0], row[4], window = n0_btn)
 
 def decimal():
-    entry.insert(tk.END, '.')
+    entry.insert(tk.INSERT, '.')
 
 decimal_btn = tk.Button(text = '.', command = decimal, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[1], row[4], window = decimal_btn)
@@ -163,19 +163,19 @@ def equal():
             equation = equation + ')'
     result = str(eval(equation))
     entry.delete(0, tk.END)
-    entry.insert(tk.END, '=' + result)
+    entry.insert(tk.END, result)
 
 equal_btn = tk.Button(text = '=', command = equal, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[2], row[4], window = equal_btn)
 
 def lbrct():
-    entry.insert(tk.END, '(')
+    entry.insert(tk.INSERT, '(')
 
 lbrct_btn = tk.Button(text = '(', command = lbrct, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[3], row[4], window = lbrct_btn)
 
 def rbrct():
-    entry.insert(tk.END, ')')
+    entry.insert(tk.INSERT, ')')
 
 rbrct_btn = tk.Button(text = ')', command = rbrct, font = btn_font, height = btn_h, width = btn_w)
 cvs.create_window(col[4], row[4], window = rbrct_btn)
